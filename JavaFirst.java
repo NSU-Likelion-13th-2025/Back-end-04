ae4d055c34d2afb4a6f84c9729ed787f35e90d59
minki0807 <myong1306@gmail.com>
2025-04-07 22:39:06 +0900

    [test] : 자바 제어문 실습

--- JavaFirst.java
+++ JavaFirst.java
@@ -1,5 +1,8 @@
 public class JavaFirst {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        int a = 10;
+        a %= 5; System.out.println(a);
     }
 }
